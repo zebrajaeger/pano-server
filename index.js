d7168fad204bb9860144526a200db2eb3eb9820d
Lars Brandt <brandt@silpion.de>
2023-05-01 12:18:25 +0200

    [improve] simplify and add template priorities

--- index.js
+++ index.js
@@ -5,54 +5,102 @@ const handlebars = require('handlebars');
 
 // -----
 const publicPath = path.join(__dirname, 'public');
-const defaultFile = 'index.html'
+const port = parseInt(process.env.SERVER_PORT) || 3000;
 const serverUrl = process.env.SERVER_URL || 'http://localhost:3000';
-const templateExtension = '.template.html'
+const isDebug = process.env.DEBUG || false;
 // -----
 
 const app = express();
+
+function debug(message, ...optionalParams) {
+  if (isDebug) {
+    console.log.apply(null, arguments);
+  }
+}
+
+console.log('---------');
+debug('Debug is enabled')
+console.log('serverUrl is ', serverUrl)
+console.log('port is ', port)
+console.log('publicPath is ', publicPath)
+console.log('---------');
+
+// if path = ('<dir>/' || '<dir>/index.html')
+//  -> '<dir>/index.template.p.html'
+//  -> '<dir>/index.template.m.html'
+//  -> '<dir>/index.template.html'
+//  -> '<dir>/index.html'
+//  -> 404
 app.use((req, res, next) => {
+  debug('REQUEST', req.path)
+
   if (req.path.endsWith('/') || req.path.endsWith('\\')) {
-    let newPath = path.posix.join(req.path, defaultFile);
-    res.redirect(newPath);
+    debug('  - without file')
+
+    if (!renderTemplates(req.path, res)) {
+      next();
+    }
+  } else if (path.basename(req.path) === 'index.html') {
+    debug('  - with index.html')
+
+    if (!renderTemplates(path.dirname(req.path), res)) {
+      next();
+    }
   } else {
     next();
   }
 });
 
-app.get('*.html', (req, res, next) => {
-  const originalPath = req.path;
-  const panoPath = originalPath.replace(/\.html$/, templateExtension);
-  const filePath = path.join(publicPath, panoPath);
-  const currentPath = path.dirname(originalPath);
-  const url = serverUrl + originalPath;
+function renderTemplates(reqPath, res) {
+  debug('renderTemplates', reqPath)
 
-  if (fs.existsSync(filePath)) {
-    let content = fs.readFileSync(filePath, 'utf8');
+  return renderTemplate(reqPath, 'index.template.p.html', res)
+      || renderTemplate(reqPath, 'index.template.m.html', res)
+      || renderTemplate(reqPath, 'index.template.html', res)
+      || renderTemplate(reqPath, 'index.html', res)
+}
 
-    const template = handlebars.compile(content);
-    const rendered = template({
-      serverUrl,
-      url,
-      currentPath,
-      originalPath,
-      panoPath
-    });
-    res.send(rendered);
+function renderTemplate(reqPath, templateFileName, res) {
+  debug('  - renderTemplate', reqPath, templateFileName)
 
-    res.send(content);
-  } else {
-    next();
+  const templateFilePath = path.join(publicPath, reqPath, templateFileName);
+  debug('    - templateFilePath', templateFilePath)
+
+  if (!fs.existsSync(templateFilePath)) {
+    debug('    -> no file')
+
+    return false;
   }
-});
 
+  let content = fs.readFileSync(templateFilePath, 'utf8');
+
+  const template = handlebars.compile(content);
+  const context = {
+    serverUrl,
+    url: path.join(serverUrl, reqPath, 'index.html'),
+    currentPath: reqPath,
+  };
+
+  if (isDebug) {
+    debug('    -> render', JSON.stringify(context));
+  }
+
+  const rendered = template(context);
+  res.send(rendered);
+
+  return true;
+}
+
+// all static file fom public directory
 app.use(express.static(publicPath));
 
+// not found -> 404
 app.use((req, res) => {
   res.status(404).send('Not found');
 });
 
-app.listen(3000, () => {
+// start server
+app.listen(port, () => {
   console.log(
-      `Server running at http://localhost:3000/; reachable @ ${serverUrl}`);
+      `Server running at http://localhost:${port}/; reachable @ ${serverUrl}`);
 });
